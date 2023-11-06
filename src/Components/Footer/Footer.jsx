abc7818c2ac7fa62607bdbb189849575dfa433a5
Eddysam2000 <samueledwin67@gmail.com>
2023-11-06 16:07:14 +0100

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './footer.css'
+import vide02 from '../../Assets/video2.mp4'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={vide02} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex"></div>
+      </div>
+    </section>
   )
 }
 
