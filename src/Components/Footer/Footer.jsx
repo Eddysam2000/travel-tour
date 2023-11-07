a6a106c0d9f458a93130276c6bd9cfbade9b76a8
Eddysam2000 <samueledwin67@gmail.com>
2023-11-07 14:38:16 +0100

    footer socials

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,11 @@ import './footer.css'
 import vide02 from '../../Assets/video2.mp4'
 import { FiSend } from 'react-icons/fi'
 import { MdOutlineTravelExplore } from 'react-icons/md'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillYoutube } from 'react-icons/ai'
+import { AiFillInstagram } from 'react-icons/ai'
+import { FaTripadvisor } from 'react-icons/fa'
+import { FiChevronRight } from 'react-icons/fi'
 
 const Footer = () => {
   return (
@@ -38,8 +43,110 @@ const Footer = () => {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugiat nulla incidunt! Ab voluptatem natus at? Voluptatum quibusdam suscipit et eum tempore debitis cumque repudiandae recusandae. Excepturi ipsa rem eveniet?
             </div>
 
-            <div className="footerSocials">
-              
+            <div className="footerSocials flex">
+              <AiOutlineTwitter className="icon"/>
+              <AiFillYoutube className="icon"/>
+              <AiFillInstagram className="icon"/>
+              <FaTripadvisor className="icon"/>
+            </div>
+          </div>
+          <div className="footerLinks grid">
+            {/* group one */}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+
+            </div>
+
+            {/* group two */}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+
+            </div>
+            {/* group three */}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+
             </div>
           </div>
         </div>
