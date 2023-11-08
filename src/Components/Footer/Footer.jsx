f16468a7fa65a683ae4932c19fa1e9e7cc7d77f5
Eddysam2000 <samueledwin67@gmail.com>
2023-11-08 10:23:48 +0100

    footer socials

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './footer.css'
 import vide02 from '../../Assets/video2.mp4'
 import { FiSend } from 'react-icons/fi'
@@ -8,8 +8,14 @@ import { AiFillYoutube } from 'react-icons/ai'
 import { AiFillInstagram } from 'react-icons/ai'
 import { FaTripadvisor } from 'react-icons/fa'
 import { FiChevronRight } from 'react-icons/fi'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Footer = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  }, [])
+
   return (
     <section className="footer">
       <div className="videoDiv">
@@ -18,14 +24,14 @@ const Footer = () => {
 
       <div className="secContent container">
         <div className="contactDiv flex">
-          <div className="text">
+          <div data-aos="fade-up" className="text">
             <small>KEEP IN TOUCH</small>
             <h2>Travel with us</h2>
           </div>
 
           <div className="inputDiv flex">
-            <input type="text" placeholder='Enter Email Address' />
-            <button className='btn flex' type='submit'>
+            <input data-aos="fade-up" type="text" placeholder='Enter Email Address' />
+            <button data-aos="fade-up" className='btn flex' type='submit'>
               SEND <FiSend className="icon" />
             </button>
           </div>
@@ -39,11 +45,11 @@ const Footer = () => {
               </a>
             </div>
 
-            <div className="footerPara">
+            <div data-aos="fade-up" className="footerPara">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugiat nulla incidunt! Ab voluptatem natus at? Voluptatum quibusdam suscipit et eum tempore debitis cumque repudiandae recusandae. Excepturi ipsa rem eveniet?
             </div>
 
-            <div className="footerSocials flex">
+            <div data-aos="fade-up" className="footerSocials flex">
               <AiOutlineTwitter className="icon"/>
               <AiFillYoutube className="icon"/>
               <AiFillInstagram className="icon"/>
@@ -52,7 +58,7 @@ const Footer = () => {
           </div>
           <div className="footerLinks grid">
             {/* group one */}
-            <div className="linkGroup">
+            <div data-aos="fade-up" data-aos-duration="3000" className="linkGroup">
               <span className="groupTitle">
                 OUR AGENCY
               </span>
@@ -85,69 +91,74 @@ const Footer = () => {
             </div>
 
             {/* group two */}
-            <div className="linkGroup">
+            <div data-aos="fade-up" data-aos-duration="4000" className="linkGroup">
               <span className="groupTitle">
-                OUR AGENCY
+                PARTNERS
               </span>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Services
+                Bookings
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Insurance
+                Trivago
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Agency
+                Rentals
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Tourism
+                TripAdvisor
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Payment
+                MakeMyTrip
               </li>
 
             </div>
             {/* group three */}
-            <div className="linkGroup">
+            <div data-aos="fade-up" data-aos-duration="5000" className="linkGroup">
               <span className="groupTitle">
-                OUR AGENCY
+                FLIGHTS USED
               </span>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Services
+                FlyEmirates
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Insurance
+                Qatar
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Agency
+                Lufthansa
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Tourism
+                AirFrance
               </li>
 
               <li className="footerList flex">
                 <FiChevronRight className="icon"/>
-                Payment
+                AirCanada
               </li>
 
             </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL PLATFORM</small>
+            <small>COPYRIGHT RESERVED - EDDYSAM 2023</small>
           </div>
         </div>
       </div>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './main.css'
 import img from '../../Assets/img (1).jpg'
 import img2 from '../../Assets/img (2).jpg'
@@ -11,6 +11,8 @@ import img8 from '../../Assets/img (8).jpg'
 import img9 from '../../Assets/img (9).jpg'
 import { HiOutlineLocationMarker } from 'react-icons/hi'
 import { HiOutlineClipboardCheck } from 'react-icons/hi'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 
 const Data = [
@@ -99,10 +101,14 @@ const Data = [
 
 
 const Main = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  }, [])
+
   return (
     <section className='main container section'>
       <div className="secTitle">
-        <h3 className="title">
+        <h3 data-aos="fade-right" className="title">
           Most Visited Destinations
         </h3>
       </div>
@@ -111,7 +117,7 @@ const Main = () => {
         {
           Data.map(({id, imgSrc, destTitle, location, grade, fees, description})=>{
             return(
-              <div key={id} className="singleDestination">
+              <div key={id} data-aos="fade-up" className="singleDestination">
                 <div className="imageDiv">
                   <img src={imgSrc} alt={destTitle} />
                 </div>
